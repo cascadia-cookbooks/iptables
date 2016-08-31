adc9595ace0cf69df3b999464f343565269c97e0
Colby Olson <me@colbyolson.com>
2016-08-31 15:00:22 -0700

    init

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'cop_iptables'
+maintainer 'Copious Inc.'
+maintainer_email 'engineering@copiousinc.com'
+license 'MIT'
+description 'Installs and configures iptables.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.0.1'
+
+source_url 'https://github.com/copious-cookbooks/iptables'
+issues_url 'https://github.com/copious-cookbooks/iptables/issues'
+
+supports 'ubuntu', '= 16.04'
+supports 'ubuntu', '= 14.04'
+
+depends 'apt'
