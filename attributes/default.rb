0f369f61ae2c0523b05e6882752b1efa89c84d0a
Colby Olson <me@colbyolson.com>
2016-08-31 17:22:07 -0700

    remove warning about node deprecation

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,7 @@ default['iptables']['rules'] = [
 ]
 
 # latest stable package release
-case lsb['release']
+case node['platform_version']
 when '14.04'
     default['iptables']['version'] = '1.4.21-1ubuntu1'
 when '16.04'
