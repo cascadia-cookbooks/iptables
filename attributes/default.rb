b796f99bb43ba7ac1d15d51f87af8780550b48a2
Colby Olson <me@colbyolson.com>
2016-08-31 17:38:16 -0700

    update attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,13 +3,10 @@ default['iptables']['packages'] = %w(iptables iptables-persistent)
 default['iptables']['rules_ipv4'] = '/etc/iptables/rules.v4'
 default['iptables']['rules_ipv6'] = '/etc/iptables/rules.v6'
 
-default['iptables']['rules'] = [
-]
-
 # latest stable package release
 case node['platform_version']
 when '14.04'
     default['iptables']['version'] = '1.4.21-1ubuntu1'
 when '16.04'
-    default['iptables']['version'] = '3.0.6'
+    default['iptables']['version'] = '1.6.0-2ubuntu3'
 end
