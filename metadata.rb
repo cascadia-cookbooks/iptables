3e9dc99e492cc03b7cab9a341c7c48a605ccd6f8
Colby Olson <me@colbyolson.com>
2016-11-29 22:30:07 -0800

    bump to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'engineering@copiousinc.com'
 license 'MIT'
 description 'Installs and configures iptables.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.3'
+version '0.2.0'
 
 source_url 'https://github.com/copious-cookbooks/iptables'
 issues_url 'https://github.com/copious-cookbooks/iptables/issues'
